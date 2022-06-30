8a9eb96b32477f61c3de92e6ca8afccbaa9d9042
amarrerod <alemarrerodiaz@gmail.com>
2022-06-30 13:41:57 +0100

    ✨ Finishes chapter 18

--- code/chapter_18_input_and_output/src/main.rs
+++ code/chapter_18_input_and_output/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+use std::io::{self, prelude::*, BufReader, BufWriter};
+mod jsons;
 
 fn grep<R>(target: &str, reader: R) -> io::Result<()>
 where
@@ -21,12 +22,16 @@ where
 fn main() {
     println!("Hello, world!");
 
-    let file =
-        "/Users/amarrero/Proyectos/programming_rust/code/chapter_18_input_and_output/src/main.rs";
+    let file = "";
     let f = File::open(file).unwrap();
     let k = grep("    let f = File::open(file).unwrap();", BufReader::new(f));
     let lines = BufReader::new(File::open(file).unwrap())
         .lines()
         .collect::<io::Result<Vec<String>>>();
-    println!("{:?}", lines.unwrap());
+    let resulting_lines = lines.unwrap();
+    println!("{:?}", &resulting_lines);
+    let temp_file = File::create("tmp.txt").unwrap();
+    let mut writer = BufWriter::new(temp_file);
+    writeln!(&mut writer, "{:#?}", &resulting_lines);
+    jsons::create_data();
 }
